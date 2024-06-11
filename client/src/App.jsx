77fe683b47e13d0e5a3a72e8694a26d5bc0e7705
Yashaswee Kesharwani <career.yashaswee@gmail.com>
2024-06-11 17:29:33 +0530

    update(ui): shadcn component add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/ui/mode-toggle";
 
 function App() {
 	return (
-		<>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<div>
 				<Button>Ai Mock Interview</Button>
+				<ModeToggle></ModeToggle>s
 			</div>
-		</>
+		</ThemeProvider>
 	);
 }
 
