6556c8ad46116cbd0431d76a41ea4872cc75db8d
Nainshi Verma <122629057+nainshiverma@users.noreply.github.com>
2024-07-22 07:43:38 +0530

    Update interview.js

--- server/routes/interview.js
+++ server/routes/interview.js
@@ -3,6 +3,9 @@ const { validate } = require("../middlewares/validateSchema");
 const {
 	createNewSession,
 	listInterviewSession,
+	fetchInterview,
+    	listInterviewsByUser,
+    	countInterviewsByUserID,
 } = require("../controllers/interview");
 const router = express.Router();
 
@@ -14,5 +17,8 @@ const {
 
 router.post("/", createNewSession);
 router.get("/", validate(listInterviewSessionSchema), listInterviewSession);
+router.get("/:interviewID", fetchInterview);
+router.get("/user/:interviews", listInterviewsByUser);
+router.get("/user/:countinterview", countInterviewsByUserID)
 router.get("/report");
 module.exports = router;
